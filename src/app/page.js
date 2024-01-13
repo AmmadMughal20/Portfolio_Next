395ca1b5038b63a978a57e98f01788fadd8a6274
Muhammad Ammad <ammadmughal567@gmail.com>
2024-01-13 13:30:31 +0500

    Latest Push

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,18 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+export default async function Home() {
+
+  let res = await fetch('https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam', {
+    next: { revalidate: 5 }
+  })
+
+  let result = await res.json()
+  
   return (
-    <h1>Main</h1>
+    <>
+      <h1>Main</h1>
+      <p>{result.seconds}</p>
+    </>
   )
 }
